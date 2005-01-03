3cbf6acaf96bcca52a8380ec61f5fff132acf3ac
loki <loki>
2005-01-03 04:55:09 +0000

    Added a pointer cursor when mousing over links, and reorganized the project.

---
+++ source/controls/MUForwardingTextView.h
@@ -0,0 +1,22 @@
+//
+// MUForwardingTextView.h
+//
+// Copyright (C) 2004 3James Software
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MUForwardingTextView : NSTextView
+{
+  IBOutlet NSTextView *targetTextView;
+}
+
+@end
+
+@interface NSObject (MUForwardingTextViewDelegate)
+
+- (NSCursor *) cursorForLink:(NSObject *)linkObject
+                     atIndex:(unsigned)charIndex
+                  ofTextView:(NSTextView *)aTextView;
+
+@end
