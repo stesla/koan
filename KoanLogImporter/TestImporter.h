f6b296a422667d63effa97b42445362b4bfd9da5
samuel <samuel>
2007-03-14 06:54:17 +0000

    Added unit test fixture for KoanLogImporter which uses the test-stub in Koan.  Also fixed Koan tests so they ran.  Added All Tests target in Koan.xcodeproj which runs both the Unit Tests target in that project and the Unit Tests target in KoanLogImporter.xcodeproj.

---
+++ KoanLogImporter/TestImporter.h
@@ -0,0 +1,16 @@
+//
+//  TestImporter.h
+//  KoanLogImporter
+//
+//  Created by Samuel Tesla on 3/14/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <J3Testing/J3TestCase.h>
+
+
+@interface TestImporter : J3TestCase 
+{
+}
+
+@end
---
+++ KoanLogImporter/TestImporter.m
@@ -0,0 +1,14 @@
+//
+//  TestImporter.m
+//  KoanLogImporter
+//
+//  Created by Samuel Tesla on 3/14/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestImporter.h"
+
+
+@implementation TestImporter
+
+@end
