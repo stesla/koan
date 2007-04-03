afa5ec05358a818ab7a7af1339dc1b464c0da446
tyler <tyler>
2007-04-03 19:16:15 +0000

    Style cleanup for MUDisplayTextView.

--- source/controls/MUDisplayTextView.m
+++ source/controls/MUDisplayTextView.m
@@ -7,36 +7,45 @@
 //
 // Copyright (c) 2003 Apple Computer, Inc. All rights reserved.
 //
-// IMPORTANT:   This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in
-// consideration of your agreement to the following terms, and your use, installation,
-// modification or redistribution of this Apple software constitutes acceptance of these
-// terms.  If you do not agree with these terms, please do not use, install, modify or
-// redistribute this Apple software.
+// License:
 //
-// In consideration of your agreement to abide by the following terms, and subject to these
-// terms, Apple grants you a personal, non-exclusive license, under Apple's copyrights in
-// this original Apple software (the "Apple Software"), to use, reproduce, modify and
-// redistribute the Apple Software, with or without modifications, in source and/or binary
-// forms; provided that if you redistribute the Apple Software in its entirety and without
-// modifications, you must retain this notice and the following text and disclaimers in all
-// such redistributions of the Apple Software.  Neither the name, trademarks, service marks
-// or logos of Apple Computer, Inc. may be used to endorse or promote products derived from
-// the Apple Software without specific prior written permission from Apple. Except as expressly
-// stated in this notice, no other rights or licenses, express or implied, are granted by Apple
-// herein, including but not limited to any patent rights that may be infringed by your
-// derivative works or by other works in which the Apple Software may be incorporated.
+//   IMPORTANT:   This Apple software is supplied to you by Apple Computer, Inc.
+//   ("Apple") in consideration of your agreement to the following terms, and
+//   your use, installation, modification or redistribution of this Apple
+//   software constitutes acceptance of these terms.  If you do not agree with
+//   these terms, please do not use, install, modify or redistribute this Apple
+//   software.
 //
-// The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES,
-// EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT,
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS
-// USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+//   In consideration of your agreement to abide by the following terms, and
+//   subject to these terms, Apple grants you a personal, non-exclusive license,
+//   under Apple's copyrights in this original Apple software (the "Apple
+//   Software"), to use, reproduce, modify and redistribute the Apple Software,
+//   with or without modifications, in source and/or binary forms; provided that
+//   if you redistribute the Apple Software in its entirety and without
+//   modifications, you must retain this notice and the following text and
+//   disclaimers in all such redistributions of the Apple Software.  Neither the
+//   name, trademarks, service marks or logos of Apple Computer, Inc. may be
+//   used to endorse or promote products derived from the Apple Software without
+//   specific prior written permission from Apple. Except as expressly stated in
+//   this notice, no other rights or licenses, express or implied, are granted
+//   by Apple herein, including but not limited to any patent rights that may be
+//   infringed by your derivative works or by other works in which the Apple
+//   Software may be incorporated.
 //
-// IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-//          OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
-// REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND
-// WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
-// OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//   The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES
+//   NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
+//   WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+//   PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION
+//   ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+//
+//   IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
+//   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//   INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION
+//   AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER
+//   THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
+//   OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//   DAMAGE.
 //
 
 #import "MUDisplayTextView.h"
@@ -59,50 +68,36 @@
 
 - (void) resetCursorRects
 {
-  NSAttributedString *attrString;
-  NSPoint containerOrigin;
-  NSRect visRect;
-  NSRange visibleGlyphRange, visibleCharRange, attrsRange;
+  NSRect visibleRect = NSOffsetRect ([self visibleRect], -[self textContainerOrigin].x, -[self textContainerOrigin].y);
   
-  attrString = [self textStorage];
+  NSRange visibleGlyphRange = [[self layoutManager] glyphRangeForBoundingRect: visibleRect
+                                                              inTextContainer: [self textContainer]];
+  NSRange visibleCharacterRange = [[self layoutManager] characterRangeForGlyphRange: visibleGlyphRange
+                                                                   actualGlyphRange: NULL];
   
-  containerOrigin = [self textContainerOrigin];
-  visRect = NSOffsetRect ([self visibleRect], -containerOrigin.x, -containerOrigin.y);
+  NSRange attributeRange = NSMakeRange (visibleCharacterRange.location, 0);
   
-  visibleGlyphRange = [[self layoutManager] glyphRangeForBoundingRect: visRect
-                                                      inTextContainer: [self textContainer]];
-  visibleCharRange = [[self layoutManager] characterRangeForGlyphRange: visibleGlyphRange
-                                                      actualGlyphRange: NULL];
-  
-  attrsRange = NSMakeRange (visibleCharRange.location, 0);
-  
-  while (NSMaxRange (attrsRange) < NSMaxRange (visibleCharRange))
+  while (NSMaxRange (attributeRange) < NSMaxRange (visibleCharRange))
   {
-    NSString *linkObject;
+    NSString *linkObject = [[self textStorage] attribute: NSLinkAttributeName
+                                                 atIndex: NSMaxRange (attributeRange)
+                                          effectiveRange: &attributeRange];
     
-    linkObject = [attrString attribute: NSLinkAttributeName
-                               atIndex: NSMaxRange (attrsRange)
-                        effectiveRange: &attrsRange];
+    if (!linkObject)
+      continue;
     
-    if (linkObject != nil)
+    NSCursor *cursor = [self cursorForLink: linkObject atIndex: attributeRange.location];
+    unsigned rectCount;
+    
+    NSRectArray rects = [[self layoutManager] rectArrayForCharacterRange: attributeRange
+                                            withinSelectedCharacterRange: NSMakeRange (NSNotFound, 0)
+                                                         inTextContainer: [self textContainer]
+                                                               rectCount: &rectCount];
+    
+    for (unsigned rectIndex = 0; rectIndex < rectCount; rectIndex++)
     {
-      NSCursor *cursor;
-      NSRectArray rects;
-      unsigned rectCount, rectIndex;
-      NSRect oneRect;
-      
-      cursor = [self cursorForLink: linkObject atIndex: attrsRange.location];
-      
-      rects = [[self layoutManager] rectArrayForCharacterRange: attrsRange
-                                  withinSelectedCharacterRange: NSMakeRange (NSNotFound, 0)
-                                               inTextContainer: [self textContainer]
-                                                     rectCount: &rectCount];
-      
-      for (rectIndex = 0; rectIndex < rectCount; rectIndex++)
-      {
-        oneRect = NSIntersectionRect (rects[rectIndex], [self visibleRect]);
-        [self addCursorRect: oneRect cursor: cursor];
-      }
+      NSRect oneRect = NSIntersectionRect (rects[rectIndex], [self visibleRect]);
+      [self addCursorRect: oneRect cursor: cursor];
     }
   }
 }
