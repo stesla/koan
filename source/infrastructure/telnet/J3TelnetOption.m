7935e74073ff30d2df453672d377a7d06eb1e958
samuel <samuel>
2007-04-11 06:18:10 +0000

    re#10 -- Removed some duplication

--- source/infrastructure/telnet/J3TelnetOption.m
+++ source/infrastructure/telnet/J3TelnetOption.m
@@ -8,6 +8,17 @@
 
 #import "J3TelnetOption.h"
 
+@interface J3TelnetOption (Private)
+
+- (void) demandDisableState: (J3TelnetQState *) state
+              ifAcknowledge: (SEL) acknowledge
+                    ifAllow: (SEL) allow;
+- (void) sendDo;
+- (void) sendDont;
+- (void) sendWill;
+- (void) sendWont;
+
+@end
 
 @implementation J3TelnetOption
 
@@ -42,27 +53,9 @@
 
 - (void) heDont
 {
-  switch (us)
-  {
-    case J3TelnetQNo:
-      break;
-      
-    case J3TelnetQYes:
-      [delegate wont: option];
-      [self setUs: J3TelnetQNo];
-      break;
-      
-    case J3TelnetQWantNoOpposite:
-      [self setUs: J3TelnetQWantYesEmpty];
-      [delegate will: option];
-      break;
-      
-    case J3TelnetQWantNoEmpty:
-    case J3TelnetQWantYesEmpty:
-    case J3TelnetQWantYesOpposite:
-      [self setUs: J3TelnetQNo];
-      break;
-  }
+  [self demandDisableState: &us 
+             ifAcknowledge: @selector (sendWont) 
+                   ifAllow: @selector (sendWill)];
 }
 
 - (void) heWill
@@ -71,27 +64,9 @@
 
 - (void) heWont
 {
-  switch (him)
-  {
-    case J3TelnetQNo:
-      break;
-      
-    case J3TelnetQYes:
-      [delegate dont: option];
-      [self setHim: J3TelnetQNo];
-      break;
-        
-    case J3TelnetQWantNoOpposite:
-      [self setHim: J3TelnetQWantYesEmpty];
-      [delegate do: option];
-      break;
-
-    case J3TelnetQWantNoEmpty:
-    case J3TelnetQWantYesEmpty:
-    case J3TelnetQWantYesOpposite:
-      [self setHim: J3TelnetQNo];
-      break;
-  }
+  [self demandDisableState: &him 
+             ifAcknowledge: @selector (sendDont) 
+                   ifAllow: @selector (sendDo)];
 }
 
 - (J3TelnetQState) him
@@ -115,3 +90,58 @@
 }
 
 @end
+
+#pragma mark -
+
+@implementation J3TelnetOption (Private)
+
+- (void) demandDisableState: (J3TelnetQState *) state
+              ifAcknowledge: (SEL) acknowledge
+                    ifAllow: (SEL) allow
+{
+  switch (*state)
+  {
+    case J3TelnetQNo:
+      break;
+      
+    case J3TelnetQYes:
+      *state = J3TelnetQNo;
+      [self performSelector: acknowledge];
+      break;
+      
+    case J3TelnetQWantNoOpposite:
+      *state = J3TelnetQWantYesEmpty;
+      [self performSelector: allow];
+      break;
+      
+    case J3TelnetQWantNoEmpty:
+    case J3TelnetQWantYesEmpty:
+    case J3TelnetQWantYesOpposite:
+      *state = J3TelnetQNo;
+      break;
+  }  
+}
+
+- (void) sendDo
+{
+  [delegate do: option];
+}
+
+- (void) sendDont
+{
+  [delegate dont: option];
+}
+
+- (void) sendWill
+{
+  [delegate will: option];
+}
+
+- (void) sendWont
+{
+  [delegate wont: option];
+}
+
+@end
+
+
