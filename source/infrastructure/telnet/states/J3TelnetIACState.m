a041cdf6d0e53f44be20636ba3ef5c0df2a8777d
tyler <tyler>
2007-04-15 00:37:59 +0000

    Re #54 - commented on various telnet commands' validity.
    Also added lines to explicitly handle all the telnet command codes which were
    previously falling through to the default line.

--- source/infrastructure/telnet/states/J3TelnetIACState.m
+++ source/infrastructure/telnet/states/J3TelnetIACState.m
@@ -21,26 +21,47 @@
 {
   switch (byte)
   {
+    // TODO: handle these valid commands individually.
+    case J3TelnetEndOfRecord:
+    case J3TelnetNoOperation:
+    case J3TelnetDataMark:
+    case J3TelnetBreak:
+    case J3TelnetInterruptProcess:
+    case J3TelnetAbortOutput:
+    case J3TelnetAreYouThere:
+    case J3TelnetEraseCharacter:
+    case J3TelnetEraseLine:
+    case J3TelnetGoAhead:
+      // TODO: these complete a valid telnet command.
+      return [J3TelnetTextState state];
+      
+    case J3TelnetBeginSubnegotiation:
+      // TODO: subnegotiations are valid telnet if and only if we've negotiated
+      // to use the corresponding option.
+      return [J3TelnetSubnegotiationState state];
+      
+    case J3TelnetWill:
+      // TODO: for all the option state modifiers, whatever follows is valid,
+      // since we want to allow for unsupported options that we don't know about
+      // without bailing on telnet.
+      return [J3TelnetWillState state];
+      
+    case J3TelnetWont:
+      return [J3TelnetWontState state];
+      
     case J3TelnetDo:
       return [J3TelnetDoState state];
       
     case J3TelnetDont:
       return [J3TelnetDontState state];
-    
-    case J3TelnetWill:
-      return [J3TelnetWillState state];
-    
-    case J3TelnetWont:
-      return [J3TelnetWontState state];
-
+      
     case J3TelnetInterpretAsCommand:
       [engine bufferInputByte: J3TelnetInterpretAsCommand];
       return [J3TelnetTextState state];
       
-    case J3TelnetBeginSubnegotiation:
-      return [J3TelnetSubnegotiationState state];
-        
+    case J3TelnetEndSubnegotiation:
     default:
+      // TODO: this is invalid telnet.
       return [J3TelnetTextState state];
   }
 }
