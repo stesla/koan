8a5bde62e3129711ad56db64c558cd4b25379065
samuel <samuel>
2007-06-06 22:23:08 +0000

    re#53 -- Configure app to use database sessions instead of file-system sessions

--- www/db/schema.rb
+++ www/db/schema.rb
@@ -2,6 +2,15 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 1) do
+
+  create_table "sessions", :force => true do |t|
+    t.column "session_id", :string
+    t.column "data",       :text
+    t.column "updated_at", :datetime
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
