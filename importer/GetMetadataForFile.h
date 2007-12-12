d1bdf5d18ce09727c0150b59c574586faa779f78
tyler <tyler>
2007-12-12 01:32:52 +0000

    Added missing header file.

---
+++ importer/GetMetadataForFile.h
@@ -0,0 +1,13 @@
+//
+// GetMetadataForFile.h
+//
+// Copyright (c) 2007 3James Software
+//
+
+#ifndef KOAN_GET_METADATA_H
+#define KOAN_GET_METADATA_H
+
+extern Boolean GetMetadataForFile (void *thisInterface, CFMutableDictionaryRef attributes, 
+                                   CFStringRef contentTypeUTI, CFStringRef pathToFile);
+
+#endif /* KOAN_GET_METADATA_H */
